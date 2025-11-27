7d0c61ba355240ecf855357e570304f8ca6b0c2c
kristiulechka <ggskristian@gmail.com>
2025-11-28 00:53:40 +0100

    updated navigation liquid glass

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -15,22 +15,24 @@ const SVGFilters = () => (
     <defs>
       <radialGradient id="lensGradient" cx="50%" cy="50%" r="50%">
         <stop offset="0%" stopColor="white" stopOpacity="1" />
+        <stop offset="30%" stopColor="white" stopOpacity="0.8" />
+        <stop offset="70%" stopColor="white" stopOpacity="0.3" />
         <stop offset="100%" stopColor="white" stopOpacity="0" />
       </radialGradient>
     </defs>
     <filter id="lensFilter" x="-50%" y="-50%" width="200%" height="200%">
       <feImage href="#lensGradient" result="grad" />
       
-      <feGaussianBlur in="SourceAlpha" stdDeviation="20" result="blur" />
+      <feGaussianBlur in="SourceAlpha" stdDeviation="15" result="blur" />
       
       <feColorMatrix in="blur" type="matrix" 
         values="1 0 0 0 0
                 0 1 0 0 0
                 0 0 1 0 0
-                0 0 0 20 -10" result="displacementMap" />
+                0 0 0 25 -12" result="displacementMap" />
       
       <feDisplacementMap in="SourceGraphic" in2="displacementMap" 
-        scale="50" xChannelSelector="A" yChannelSelector="A" />
+        scale="15" xChannelSelector="A" yChannelSelector="A" />
     </filter>
   </svg>
 );
